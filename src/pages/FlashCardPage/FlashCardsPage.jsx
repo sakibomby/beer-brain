ebda715fd8c9269a8648e3347f87c8ab7962c8fc
sakibomby <csakae@gmail.com>
2024-05-15 21:41:17 -0700

    Refactor Flash Card

--- src/pages/FlashCardPage/FlashCardsPage.jsx
+++ src/pages/FlashCardPage/FlashCardsPage.jsx
@@ -1,38 +1,29 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import * as beersApi from '../../utilities/beers-api';
-import FlashCardList from "../../components/FlashCardList/FlashCardList";
+import FlashCard from '../../components/FlashCard/FlashCard';
 
 export default function FlashCardsPage() {
     const [myFlashCards, setMyFlashCards] = useState([]);
-    const [activeStyle, setActiveStyle] = useState('');
-    const stylesRef = useRef([]);
-
     useEffect(() => {
         async function getBeers() {
             const cards = await beersApi.getAllMyBeers();
-            stylesRef.current = [...new Set(cards.map(card => card.style.name))];
             setMyFlashCards(cards);
-            setActiveStyle(stylesRef.current[0]);
         }
         getBeers();
     } , []);
 
-    // const flashCards = myFlashCards.map((c) => <FlashCard 
-    //     card={c}
-    //     activeStyle={activeStyle} 
-    //     key={c._id} 
-    //     /> 
-    // )
+    const flashCards = myFlashCards.map((c) => <FlashCard 
+        card={c} 
+        key={c._id} 
+        /> 
+    )
 
     return (
         <main>
             <h1>Flash Cards</h1>
-            
-                {/* {flashCards} */}
-            <FlashCardList
-        myFlashCards={myFlashCards.filter(card => card.style.name === activeStyle)}
-      />
-            
+            <ul>
+                <div>{flashCards}</div>
+            </ul>
         </main>
     )
 }
